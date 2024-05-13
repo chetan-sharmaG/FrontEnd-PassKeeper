f9da52141fa77afe23e7b86219b24cd1a5e3570a
unknown <chetansharma2-deloitte>
2024-05-13 17:26:11 +0530

    frontend PassKeeper2

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId="676256167848-geg7fsv93inhda860s6c5k6d42g4qngd.apps.googleusercontent.com">
-   
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
   </GoogleOAuthProvider>,
 )
